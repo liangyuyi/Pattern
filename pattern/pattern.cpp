3732837c66e9d19f91ec8d7fb188f044cd752388
Tony <tony@gmail.com>
2017-04-16 23:02:48 +0800

    liangyuyi

--- pattern/pattern.cpp
+++ pattern/pattern.cpp
@@ -6,13 +6,18 @@
 //main.cpp
 #include "Factory.h" 
 #include "Product.h"
+#include "AbstractFactory.h"
 #include <iostream> 
 using namespace std;
 
 int main()
 {
-	Factory* fac = new ConcreteFactory();
-	Product* p = fac->CreateProduct();
+	AbstractFactory* cf1 = new ConcreteFactory1();
+	cf1->CreateProductA();
+	cf1->CreateProductB();
+	AbstractFactory* cf2 = new ConcreteFactory2();
+	cf2->CreateProductA(); 
+	cf2->CreateProductB();
 	return 0;
 }
 
@@ -20,16 +25,31 @@ int main()
 void menu()
 {
 	cout << "1.Factory设计模式" << endl;
+	cout << "2.AbstractFactory设计模式" << endl;
 	int x;
 	cin >> x;
 	switch (x)
 	{
 	case 1:
-		Factory* fac = new ConcreteFactory();
-		Product* p = fac->CreateProduct();
-		break;
+	{
+			  Factory* fac = new ConcreteFactory();
+			  Product* p = fac->CreateProduct();
+			  break;
+	}
+	case 2:
+	{
+			  AbstractFactory* cf1 = new ConcreteFactory1();
+			  cf1->CreateProductA();
+			  cf1->CreateProductB();
+			  AbstractFactory* cf2 = new ConcreteFactory2();
+			  cf2->CreateProductA();
+			  cf2->CreateProductB();
+			  break;
+	}
 	default:
-		cout << "you select is error" << endl;
-		break;
+	{
+			   cout << "you select is error" << endl;
+			   break;
+	}
 	}
 }
